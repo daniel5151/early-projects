089ef104f33aada46d34a192af2bfb33dfa976b3
daniel5151 <danielprilik@gmail.com>
2014-03-24 19:49:36 -0400

    Physics Thingy

---
+++ utilityFuncs.js
@@ -0,0 +1,29 @@
+// UTILITY FUNCTIONS
+function getRandomColor() {
+    var letters = '0123456789ABCDEF'.split('');
+    var color = '#';
+    for (var i = 0; i < 6; i++ ) {
+        color += letters[Math.round(Math.random() * 15)];
+    }
+    return color;
+}
+function lineDistance( point1, point2 ) {
+  var xs = 0;
+  var ys = 0;
+
+  xs = point2[0] - point1[0];
+  xs = xs * xs;
+
+  ys = point2[1] - point1[1];
+  ys = ys * ys;
+
+  return Math.sqrt( xs + ys );
+}
+
+// Return what object has been clicked
+function whatObjClick () {
+    for (var key in objects) {
+        if (objects[key].checkClick()) { return objects[key]; }
+    }
+    return null;
+}
